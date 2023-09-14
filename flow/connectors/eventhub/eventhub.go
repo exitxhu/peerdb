3d416d00942dbd5fae71aa5f183bec65ba64b695
Amogh-Bharadwaj <amogh@peerdb.io>
2023-09-14 18:24:45 +0530

    locks for tablemapping

--- flow/connectors/eventhub/eventhub.go
+++ flow/connectors/eventhub/eventhub.go
@@ -207,6 +207,7 @@ func (c *EventHubConnector) sendEventBatch(events map[string][]*eventhub.Event,
 	var wg sync.WaitGroup
 	var once sync.Once
 	var firstErr error
+	var mapLock sync.Mutex
 	// Limiting concurrent sends
 	guard := make(chan struct{}, maxParallelism)
 
@@ -239,7 +240,9 @@ func (c *EventHubConnector) sendEventBatch(events map[string][]*eventhub.Event,
 				"flowName": flowName,
 			}).Infof("pushed %d events to event hub: %s",
 				numEventsPushed, tblName)
+			mapLock.Lock()
 			tableNameRowsMapping[tblName] += uint32(len(eventBatch))
+			mapLock.Unlock()
 		}(tblName, eventBatch)
 	}
 
